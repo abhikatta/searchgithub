ad0364ca8b6fa09c3eed7b84fdbb921cef064112
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-06-20 09:08:46 +0530

    .

--- gitter.sh
+++ gitter.sh
@@ -1,14 +1,30 @@
-#!/bin/bash
+# take an input message if it is empty
+# take another input either y/n
+# if y then no npm run deploy
+# else yes
 
-if [ "$arg1" == "" ]; then
-    echo "Enter the commit message: "
-    read commit_message
-fi
-if [ "$arg2" == "n" ]; then
+echo "Enter commit message: "
+read arg1
+
+echo "Push to prod?(y/n): "
+read arg2
+
+while [-z "$arg1"] || [-z "$arg2" ]; do
+    echo "Enter commit message: "
+    read arg1
+
+    echo "Push to prod?(y/n): "
+    read arg2
+done
+
+if [$arg2=='n']; then
+    git add .
+    git commit -m "$arg1"
+    git push
+
+else
+    git add .
+    git commit -m "$arg1"
+    git push
     npm run deploy
 fi
-
-git add .
-git commit -m "$commit_message"
-git push
-echo "Done"
